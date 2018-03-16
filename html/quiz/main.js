146a907bc83482be03e4de3b9d3af6d2217cd6ff
christophergust <christophergust@gmail.com>
2018-03-16 00:23:53 -0400

    Chapter 3

--- html/quiz/main.js
+++ html/quiz/main.js
@@ -1,5 +1,25 @@
 alert('Welcome to Quiz Ninja!');
 
-const question = "What is Superman's real name?";
+/*const question = "What is Superman's real name?";
 const answer = prompt(question);
-alert(`You answered ${answer}`);
+alert(`You answered ${answer}`);*/
+
+  const quiz = [["What's Superman's real name?", "Clark Kent"],
+                ["What's Wonder Woman's real name?", "Diana Prince"],
+                ["What's Batman's real name?", "Bruce Wayne"]];
+
+let score = 0; // initialize score
+
+for (const [question, answer] of quiz){
+  const response = prompt(question);
+  if(response === answer){
+    alert('Correct!');
+    score++;
+  }
+  else{
+    alert(`Wrong!  The correct answer was ${answer}`);
+  }
+}
+
+// At the end of the game report the player's score)
+alert(`Game over!  You scored ${score} point${score !== 1 ? 's' : ''}`);
